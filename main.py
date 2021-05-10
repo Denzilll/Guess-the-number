0b4f18c138c3e26888ef37d464a6a43ebd378e09
DaPepper <67978657+DaPepper@users.noreply.github.com>
2021-05-10 17:07:25 +0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,15 +1,24 @@
 import random
 print('Привет! Попробуй отгадать число от 1 до 100.')
 number = random.randint(1, 100)
+levelselect = int(input('Введите сколько Вам нужно попыток (если вы достигните этого количества, то проиграете)' ))
+level = levelselect
 info = int(input('Введите число от 1 до 100: '))
 count = 0
+victory = 1
 while info != number:
 	count += 1
+	if level == count:
+		victory =- 1
+		break
 	if number > info:
 		print("Число больше")
 	elif number < info:
 		print('Число меньше')
 	info = int(input('Попробуй еще раз: '))
-print('Вы угадали, я загадал число', number)
-print("Вы затратили всего", count, 'попыток!')
+if victory == 1:
+	print('Вы угадали, я загадал число', number)
+	print("Вы затратили всего", count, 'попыток!')
+else:
+	print('Вы проиграли :(')
 input("Нажмите Enter для выхода")
